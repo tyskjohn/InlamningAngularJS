a8e5d01ce277df39b25cdbe8e2438db55ef30466
Johan Tysk <johan.tysk@hotmail.se>
2019-04-05 11:07:58 +0200

    shoppingcart JS added

--- js/controllers.js
+++ js/controllers.js
@@ -33,5 +33,46 @@ app.controller("cardController", function ($scope, $http) {
 
         return starProcentageRounded;
     }
+
+    // // ShoppingCart
+
+    // let getItemId = function(itemList, id) {
+    //     return _.find(itemList, function(product) {
+    //         return product.id === id
+    //     });
+    // };
+
+    // $scope.cart = [];
+
+    // $scope.addToCart = function(product) {
+    //     let found = getItemId($scope.cart, product.id);
+    //     if (found) {
+    //         found.quantity += product.quantity;
+    //     } else {
+    //         $scope.cart.push(angular.copy(product)); 
+    //     };
+    // };
+
+    // $scope.removeItem = function(product) {
+    //     let index = $scope.cart.indexOf(product);
+    //     $scope.cart.splice(index,1);
+    // };
+
+    // $scope.getItemCost = function(product) {
+    //     return product.quantity * product.price;
+    // };
+
+    // $scope.getTotalPrice = function() {
+    //     let total = _.reduce($scope.cart, function(sum, product) {
+    //         return sum + $scope.getItemCost(product);
+    //     }, 0 );
+    //     if ($scope.cart.length === 0 ) {
+    //         $scope.emptycart = "Kundvagnen är tom"
+    //     } else {
+    //         $scope.emptycart = ""
+    //     }
+    //     return total;
+    // };
+
     
 });
